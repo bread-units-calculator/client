d2c27c4f4eb2d1e9652a133bc999c02fd0743e0d
Andrew Serputko <aserputko@gmail.com>
2015-02-16 00:13:10 +0200

    v1

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip('' + __dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
